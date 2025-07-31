1d9ac7f2bfddc190abe2c201fa10c75d4f80ca74
Galvas <galvaomercante@gmail.com>
2025-07-31 13:09:52 -0300

    Create: Controller with Scaffolding

--- AgendaBarbeiros/Program.cs
+++ AgendaBarbeiros/Program.cs
@@ -1,9 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using AgendaBarbeiros.Data;
+
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<AgendaBarbeirosContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("AgendaBarbeirosContext") ?? throw new InvalidOperationException("Connection string 'AgendaBarbeirosContext' not found.")));
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AppDbContext") ?? throw new InvalidOperationException("Connection string 'AppDbContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
