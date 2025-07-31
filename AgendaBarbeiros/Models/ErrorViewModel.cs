7cb776571def759b0f0f17cbc39c18f23890cf41
Galvas <galvaomercante@gmail.com>
2025-07-31 11:58:39 -0300

    Ignorando arquivos temporários do VS

---
+++ AgendaBarbeiros/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AgendaBarbeiros.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
